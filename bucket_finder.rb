071dc762640b54e49bd46f4e77f810d14708fc9f
kpatenaude <kpatenaude@opus45.localdomain>
2022-02-04 12:07:20 -0600

    Add AWS Gov Cloud

--- bucket_finder.rb
+++ bucket_finder.rb
@@ -262,6 +262,8 @@ case region
 		host = ('http://s3-ap-southeast-1.amazonaws.com')
 	when "to"
 		host = ('http://s3-ap-northeast-1.amazonaws.com')
+        when "gov"
+                host = ('http://s3-us-gov-west-1.amazonaws.com')
 	else
 		puts "Unknown region specified"
 		puts
